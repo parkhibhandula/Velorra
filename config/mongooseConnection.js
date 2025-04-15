d45a2ad2dc2c219f1824384774077ea2f85dcb71
parkhibhandula <parkhibhandula@gmail.com>
2025-04-15 23:40:44 +0530

    config and debugger added

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const dbg = require('debug')('development:mongoose');
+const config = require('config');
 mongoose.
-connect("mongodb://127.0.0.1:27017/velorra")
+connect(`${config.get('MONGODB_URI')}/velorra`)
 .then(function(){
-    console.log('Connection setup');
+   dbg('Connection setup');
 })
 .catch((err)=>{
-    console.log(err);
+   dbg(err);
 })
 module.exports = mongoose.connection;
--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner');
 router.get('/',(req,res)=>{
-    res.send("Hello");
+    res.send("Hello Owner");
 });
 
 module.exports = router;
